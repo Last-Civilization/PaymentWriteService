f8d80bdcdec071a7150faea8fc30a505e3e8f67b
mrrevo55 <kwolny31@gmail.com>
2022-09-17 20:59:34 +0200

    Initial commit

---
+++ src/main/java/com/lastcivilization/paymentwriteservice/PaymentWriteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lastcivilization.paymentwriteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentWriteServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentWriteServiceApplication.class, args);
+    }
+
+}
