a1760bd89426f9ea204f22848296d68ca6742026
mrrevo55 <kwolny31@gmail.com>
2022-09-18 19:47:31 +0200

    Remove classes which tests feing client get data

--- src/main/java/com/lastcivilization/paymentwriteservice/domain/dto/UserDto.java
+++ src/main/java/com/lastcivilization/paymentwriteservice/domain/dto/UserDto.java
@@ -1,13 +1,7 @@
 package com.lastcivilization.paymentwriteservice.domain.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserDto {
 
-    @JsonProperty("keycloakId")
     private String keycloakId;
-    @JsonProperty("account")
     private Long account;
 }
--- src/main/java/com/lastcivilization/paymentwriteservice/infrastructure/application/config/SecurityConfiguration.java
+++ src/main/java/com/lastcivilization/paymentwriteservice/infrastructure/application/config/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package com.lastcivilization.paymentwriteservice.infrastructure.application;
+package com.lastcivilization.paymentwriteservice.infrastructure.application.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
