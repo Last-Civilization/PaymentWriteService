543fa9dd70ea7c7e94d7ea7f1e7ca0d6a5737445
mrrevo55 <kwolny31@gmail.com>
2022-11-02 21:05:48 +0100

    Refactored code

--- src/main/java/com/lastcivilization/paymentwriteservice/domain/Mapper.java
+++ src/main/java/com/lastcivilization/paymentwriteservice/domain/Mapper.java
@@ -2,7 +2,11 @@ package com.lastcivilization.paymentwriteservice.domain;
 
 import com.lastcivilization.paymentwriteservice.domain.view.AccountModel;
 
-class Mapper {
+final class Mapper {
+
+    private Mapper(){
+        throw new RuntimeException("You can not create instance of this class!");
+    }
 
     static AccountModel toDto(Account account){
         return new AccountModel(
